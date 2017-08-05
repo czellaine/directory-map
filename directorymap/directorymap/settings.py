641afe3a3287794ad907fbdec8f00cc946722a31
Czelina Ellaine Litimco <czel@Czelinas-MacBook-Air.local>
2017-08-05 15:07:28 +0800

    DB changes`

--- directorymap/directorymap/settings.py
+++ directorymap/directorymap/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,17 +85,19 @@ WSGI_APPLICATION = 'directorymap.wsgi.application'
 #         'PORT': '3306',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'directory_map',
+#         'USER': 'czel',
+#         'PASSWORD': 'czel1234',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'directory_map',
-        'USER': 'czel',
-        'PASSWORD': 'czel1234',
-        'HOST': 'localhost',
-        'PORT': '',
-    }
+    'default': dj_database_url.config()
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
